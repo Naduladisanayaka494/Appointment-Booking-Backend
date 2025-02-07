50834df7e82e5583ba5a1baa9f6ec125f2dc15c7
Naduladisanayaka494 <nadula@cypsolabs.com>
2025-02-07 16:52:30 +0530

    cre:folder structure created

---
+++ src/main/java/com/AppointmentBookingSystem/Appointment/Booking/System/services/jwt/UserService.java
@@ -0,0 +1,8 @@
+package com.AppointmentBookingSystem.Appointment.Booking.System.services.jwt;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService {
+
+    UserDetailsService userDetailService();
+}
